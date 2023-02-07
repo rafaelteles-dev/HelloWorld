bd05e46acfb02e836752c70260731220e10dd173
Rafael Teles <rafaeltelesalbertonii@gmail.com>
2023-02-07 20:07:50 -0300

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Serilog;
+
+namespace HelloWorld
+{
+    public class Pessoa
+    {
+        public string nome;
+
+    }
+    class Program
+    {
+        static string Pessoas()
+        {
+            string nome = "rafael";
+            int idade = 31;
+            return nome;
+        }
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        
+            Log.Information("Hello, World!");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
